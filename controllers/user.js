023bfa21679a15ceb927a0d7d832c0c8c7ebafb1
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-11-12 14:24:09 -0600

    Heroku.

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,23 @@
+const User = require("../models").User;
+const Profile = require("../models").UserProfile;
 
+const constants = require("../constants");
 
+const getUserData = (req, res) => {
+    User.findByPk(req.user.id, {
+        include: [
+            {
+                model: Profile
+            }
+        ]
+    })
+    .then(userData => {
+        res.status(constants.SUCCESS).json(userData)
+    })
+    .catch(err => {
+        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    })
+}
 module.exports = {
-    
+    getUserData
 }
