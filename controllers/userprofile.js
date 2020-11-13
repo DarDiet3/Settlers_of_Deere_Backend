d0ed4654cacd3177f9bba02c9a2646b362f360bf
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-11-13 15:22:41 -0600

    try to resolve

--- controllers/userprofile.js
+++ controllers/userprofile.js
@@ -1,5 +1,5 @@
 const User = require("../models").User;
-const Profile = require("../models").UserProfile;
+const Profile = require("../models").Userprofile;
 
 const constants = require("../constants");
 
